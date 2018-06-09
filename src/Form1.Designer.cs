69c1b6468aadc6adaa8d4e7f7f55cce371c1ad4d
Timothy Johnson <tandrewjohnson@outlook.com>
2018-06-08 22:47:13 -0400

    Automatically download and extract wallpaper images on start

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -28,36 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.logTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(25, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(261, 22);
-            this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 76);
+            this.button1.Location = new System.Drawing.Point(324, 24);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(132, 44);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Set Location";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(28, 32);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(28, 90);
+            this.logTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.Size = new System.Drawing.Size(428, 138);
+            this.logTextBox.TabIndex = 2;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(486, 253);
+            this.Controls.Add(this.logTextBox);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "WinDynamicDesktop Settings";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -65,8 +84,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox logTextBox;
     }
 }
 
--- src/Form1.cs
+++ src/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
 
 namespace WinDynamicDesktop
 {
     public partial class Form1 : Form
     {
+        private Uri imagesZipUri = new Uri("https://files.rb.gd/mojave_dynamic.zip");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void updateLogLine(string newLine)
+        {
+            string[] lines = logTextBox.Lines;
+            lines[lines.Length - 1] = newLine;
+            logTextBox.Lines = lines;
+        }
+
+        public void downloadImagesZip()
+        {
+            logTextBox.Text += "Downloading images.zip (39.0 MB)...0%";
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadProgressChanged += client_DownloadProgressChanged;
+                client.DownloadFileCompleted += client_DownloadFileCompleted;
+                client.DownloadFileAsync(imagesZipUri, "images.zip");
+            }
+        }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            updateLogLine("Downloading images.zip (39.0 MB)..." + e.ProgressPercentage + "%");
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            updateLogLine("Downloading images.zip (39.0 MB)...done" + Environment.NewLine);
+            extractImagesZip();
+        }
+
+        public void extractImagesZip()
+        {
+            logTextBox.Text += "Extracting images.zip...";
+            ZipFile.ExtractToDirectory("images.zip", "images");
+            logTextBox.Text += "done" + Environment.NewLine;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (!Directory.Exists("images"))
+            {
+                if (!File.Exists("images.zip"))
+                {
+                    downloadImagesZip();
+                }
+                else
+                {
+                    extractImagesZip();
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
