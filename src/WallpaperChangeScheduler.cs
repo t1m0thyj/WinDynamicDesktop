65a284c8796f83af7c076e35b63db20e0fbc62ce
Nicholas Veal <nicholasveal@hotmail.com>
2018-06-28 19:03:34 -0400

    Update WallpaperChangeScheduler.cs

--- src/WallpaperChangeScheduler.cs
+++ src/WallpaperChangeScheduler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,6 @@ using System.Threading.Tasks;
 using System.Globalization;
 using System.IO;
 using System.Windows.Forms;
-using Microsoft.Win32;
 
 namespace WinDynamicDesktop
 {
