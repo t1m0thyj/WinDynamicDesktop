3f5740849e1198a198a18db52dc3499eb5db0033
Dasutein <dustine_2008@hotmail.com>
2019-01-17 12:20:28 +0800

    Enabled DPI compatibility for Win7/8.1 in manifest file and changed AutoScaleMode value on all forms to 'Dpi'

--- src/AboutDialog.Designer.cs
+++ src/AboutDialog.Designer.cs
@@ -133,8 +133,8 @@
             // AboutDialog
             // 
             this.AcceptButton = this.closeButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(399, 144);
             this.Controls.Add(this.creditsBox);
             this.Controls.Add(this.donateButton);
