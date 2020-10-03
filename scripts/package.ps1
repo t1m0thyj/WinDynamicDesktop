8911c95a82aa33d6bbd3b566652a0bf7fdd4eddd
Timothy Johnson <tandrewjohnson@outlook.com>
2020-10-03 01:16:32 -0400

    Fix package.ps1 error when dist dir doesn't exist

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -1,8 +1,10 @@
 Set-Location (Split-Path $MyInvocation.MyCommand.Path)
 
 $buildDir = "..\src\bin\Release"
+$distDir = "..\dist"
 $appVersion = (Get-Item -Path "$buildDir\WinDynamicDesktop.exe").VersionInfo.ProductVersion
 
-Copy-Item -Path "$buildDir\WinDynamicDesktop.exe" -Destination "..\dist\WinDynamicDesktop_$appVersion`_Portable.exe"
+New-Item -Path $distDir -ItemType Directory -Force
+Copy-Item -Path "$buildDir\WinDynamicDesktop.exe" -Destination "$distDir\WinDynamicDesktop_$appVersion`_Portable.exe"
 
 iscc .\installer.iss
