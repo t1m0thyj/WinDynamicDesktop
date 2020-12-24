ef15e7396b2fc5f23ebf85cc8149a74774b090ba
Timothy Johnson <tandrewjohnson@outlook.com>
2020-12-23 19:29:29 -0500

    Generate checksums.txt and add VirusTotal workflow for new releases

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -8,3 +8,12 @@ New-Item -Path $distDir -ItemType Directory -Force
 Copy-Item -Path "$buildDir\WinDynamicDesktop.exe" -Destination "$distDir\WinDynamicDesktop_$appVersion`_Portable.exe"
 
 iscc .\installer.iss
+
+$installerHash = (Get-FileHash -Path "$distDir\WinDynamicDesktop_$appVersion`_Setup.exe").Hash.ToLower()
+$portableHash = (Get-FileHash -Path "$distDir\WinDynamicDesktop_$appVersion`_Portable.exe").Hash.ToLower()
+$appVersion3 = $appVersion -Replace "\.0$"
+$checksumsTxt = "$distDir\checksums.txt"
+
+Remove-Item $checksumsTxt -ErrorAction Ignore
+Add-Content -Path $checksumsTxt "$installerHash  WinDynamicDesktop_$appVersion3`_Setup.exe"
+Add-Content -Path $checksumsTxt "$portableHash  WinDynamicDesktop_Portable.exe"
